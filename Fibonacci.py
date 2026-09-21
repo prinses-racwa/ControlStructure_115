9152ff5a62f5b371332689ac41ca605e9321a1ae
prinses-racwa <rajwaamelia@gmail.com>
2026-09-21 11:48:28 +0700

    finnaly

--- Fibonacci.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter the value of n: "))
-a, b = 0, 1
-
-print(f"Fibonacci series up to {n}:")
-while a <= n:
-    print(a, end=" ")
-    a, b = b, a + b
-print()
--- produceFollowingDesign.py
+++
@@ -1,4 +0,0 @@
-n = int(input("Enter the value of n: "))
-
-for i in range(1, n + 1):
-    print((str(i) + " ") * i)
