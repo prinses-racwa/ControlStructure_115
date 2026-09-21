1729543adcaf9fa6f33cc5f93b5b38abada64606
prinses-racwa <rajwaamelia@gmail.com>
2026-09-21 11:53:47 +0700

    perbaikan

---
+++ evaluatethestudentperformance.py
@@ -0,0 +1,12 @@
+nilai = int(input("masukkan nilai: "))
+
+if nilai >= 90:
+    print("exellent")
+elif nilai >= 80:
+    print("very good")
+elif nilai >= 70:
+    print("good")
+elif nilai >= 60:
+    print("average")
+else:
+    print("poor")
